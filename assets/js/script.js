3130e4d973836911c0980b9ab3970e628d2ae078
Andrea Flemings <andreaflemings@andreas-mbp.mynetworksettings.com>
2022-06-12 23:33:52 -0400

    added event default feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
 //assigning button element obj. rep to a variable
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function () {
+var createTaskHandler = function (event) {
+
+    event.preventDefault();
     var listItemEl = document.createElement("li");
     listItemEl.classname = "task-item";
     listItemEl.textContent = "This is a new task.";
@@ -12,4 +14,6 @@ var createTaskHandler = function () {
 
 
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);//file finds <form> element in page and saves it vae formEl so we can interact with form and access some of its chile HTML elements
+
+
